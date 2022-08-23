51b10efa6378ea0c69de0f72b38431acfc46f4e1
nxoo <nxoo.dev@gmail.com>
2022-08-23 12:08:46 +0300

    todo list works

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-  //console.log("The DOM has loaded");
-document.querySelector('form').addEventListener('submit',(e)=>{
-  e.preventDefault()
-  createMyTodo(e.target.task.value);
-  console.log
-})
+let taskForm = document.querySelector('#task')
+let addTask = document.querySelector('#add')
+let ul = document.querySelector('#ul')
+let tasks = []
 
+function displayTasks() {
+    ul.innerHTML = ''
+    for (let x=0; x<tasks.length; x++) {
+        let li = document.createElement('li')
+        li.innerHTML = `${tasks[x]} <button onclick="deleteTasks(${x})">x</button>`
+        ul.appendChild(li)
+    }
+}
 
-});
+addTask.onclick = function(e) {
+    e.preventDefault();
+    if (taskForm.value) {
+        tasks.push(taskForm.value)
+        taskForm.value = ''
+    }
+    displayTasks()
+}
+
+function deleteTasks(x) {
+    tasks.splice(x, 1)
+    displayTasks()
+}
