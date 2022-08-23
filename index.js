46b9cdf72083ce4d199bd056fe2d4113b48b6dbd
nxoo <nxoo.dev@gmail.com>
2022-08-23 12:29:17 +0300

    comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let taskForm = document.querySelector('#task')
 let addTask = document.querySelector('#add')
 let ul = document.querySelector('#ul')
-let tasks = []
+let tasks = []    // array to store all the tasks
 
 function displayTasks() {
-    ul.innerHTML = ''
+    ul.innerHTML = ''    // clear the ul list to prevent appending elements and list again afresh
     for (let x=0; x<tasks.length; x++) {
         let li = document.createElement('li')
         li.innerHTML = `${tasks[x]} <button onclick="deleteTasks(${x})">x</button>`
@@ -13,15 +13,15 @@ function displayTasks() {
 }
 
 addTask.onclick = function(e) {
-    e.preventDefault();
-    if (taskForm.value) {
-        tasks.push(taskForm.value)
-        taskForm.value = ''
+    e.preventDefault();    // prevent page from refreshing and clearing the tasks array
+    if (taskForm.value) {    // checks if task form is not empty
+        tasks.push(taskForm.value)    // add task form value to tasks array
+        taskForm.value = ''    // clear the task form
     }
     displayTasks()
 }
 
 function deleteTasks(x) {
-    tasks.splice(x, 1)
+    tasks.splice(x, 1)    // delete tasks element by index
     displayTasks()
 }
